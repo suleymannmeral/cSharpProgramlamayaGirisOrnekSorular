081a60eb7ebafee6ccba5a02ca5b9666af961671
Suleyman Meral <mrlslymn02@gmail.com>
2025-11-13 23:04:28 +0300

    Soru 3

---
+++ Soru_3.cs
@@ -0,0 +1,18 @@
+﻿//Kullanıcıdan sıcaklık değeri isteyiniz. Sıcaklık 0 değerinin altında ise “Soğuk”, 
+//0 ile 10 derecenin arasında ise “ılık”, 10 derecenin üstünde ise konsol ekranına “sıcak” yazdırınız.   
+
+Console.Write("Sıcaklık değerini giriniz: ");
+double sicaklik = Convert.ToDouble(Console.ReadLine());
+
+if (sicaklik < 0)
+{
+    Console.WriteLine("Soğuk");
+}
+else if (sicaklik >= 0 && sicaklik <= 10)
+{
+    Console.WriteLine("Ilık");
+}
+else // sicaklik > 10
+{
+    Console.WriteLine("Sıcak");
+}
