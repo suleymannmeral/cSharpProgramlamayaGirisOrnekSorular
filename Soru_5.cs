2b16772bd7acf49e7074c6378347c4c85d0c442f
Suleyman Meral <mrlslymn02@gmail.com>
2025-11-15 11:19:27 +0300

    Soru 5

---
+++ Soru_5.cs
@@ -0,0 +1,10 @@
+﻿// 1’den 20’ye kadar olan sayıları ekrana yazdıran bir program yazınız. 
+// Her bir sayı için, sayı tek ise yanına “tektir”, çift ise “çifttir” yazdırınız.
+
+for (int i = 1; i <= 20; i++)
+{
+    if (i % 2 == 0)
+        Console.WriteLine($"{i} çifttir");
+    else
+        Console.WriteLine($"{i} tektir");
+}
