98c9fcf1e8de0bd8553500b50f05590576d1ee90
Suleyman Meral <mrlslymn02@gmail.com>
2025-11-15 12:08:00 +0300

    Soru 6

---
+++ Soru_6.cs
@@ -0,0 +1,13 @@
+﻿// Kullanıcıdan 5 tane sayı girmesini isteyen ve bu sayıların toplamını ekrana yazdıran bir program yazınız. 
+
+int[] sayilar = new int[5];
+int toplam = 0;
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Write($"{i + 1}. sayıyı girin: ");
+    sayilar[i] = int.Parse(Console.ReadLine());
+    toplam += sayilar[i];
+}
+
+Console.WriteLine("Toplam: " + toplam);
