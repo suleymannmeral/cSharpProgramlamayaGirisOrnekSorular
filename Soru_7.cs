b0649cb50fef36d5300789da3fd388ce61b35fe0
Suleyman Meral <mrlslymn02@gmail.com>
2025-11-15 12:22:40 +0300

    Soru 7

---
+++ Soru_7.cs
@@ -0,0 +1,24 @@
+﻿// Kullanıcıdan vize ve final notunu isteyiniz. Ortalaması 60 ve 60 dan büyük ise geçti,
+// 60’dan küçük ise ekrana kaldı yazınız. Beklenen yapı aşağıda verilmiştir.
+
+// Vize notunu girin: 80
+// Final notunu girin: 100
+// Ortalamanız: 92 Geçtiniz
+
+
+Console.Write("Vize notunu girin: ");
+int vize = int.Parse(Console.ReadLine());
+
+Console.Write("Final notunu girin: ");
+int final = int.Parse(Console.ReadLine());
+
+double average = vize*0.4 + final*0.6;
+
+if (average < 60)
+{
+    Console.WriteLine($"Ortalamanız: {average} Kaldınız");
+}
+else
+{
+    Console.WriteLine($"Ortalamanız: {average} Geçtiniz");
+}
